bcebcac7e7b9fe89e3e9de1d7e18c66f6ebda7aa
KornienkoAleksei <floyd.home.studio@gmail.com>
2023-03-21 16:02:07 +0400

    new day

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -13,7 +14,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -33,15 +37,11 @@ fun Home( navController: NavController, modifier: Modifier = Modifier,) {
         context.getSharedPreferences("LittleLemon", Context.MODE_PRIVATE)
     }
     Column() {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "little lemon logo",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .height(96.dp)
-                .align(Alignment.CenterHorizontally)
-        )
+       HomeHeader(navController = navController);
+        HomeHero();
+        Spacer(modifier = Modifier.height(48.dp))
+
+
         Text(
             text = "Home page",
             textAlign = TextAlign.Center,
@@ -85,6 +85,7 @@ fun Home( navController: NavController, modifier: Modifier = Modifier,) {
                 color = Color.Black,
                 modifier = Modifier
                     .fillMaxWidth()
+                    .aspectRatio(1f)
                     .wrapContentHeight(Alignment.CenterVertically)
             )
         }
@@ -94,6 +95,74 @@ fun Home( navController: NavController, modifier: Modifier = Modifier,) {
 
 @Composable
 fun HomeHeader(navController: NavController, modifier: Modifier = Modifier,){
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(96.dp),
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "little lemon logo",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .height(96.dp)
+                .align(Alignment.Center)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "profile",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .height(48.dp)
+                .padding(end = 16.dp)
+                .align(Alignment.CenterEnd)
+                .clickable { navController.navigate(Profile.route); }
+        )
+    }
+}
+
+@Composable
+fun HomeHero(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(LittleLemonGreen)
+    ) {
+        Text(
+            text = "Little Lemon",
+            style = MaterialTheme.typography.h1,
+            color = LittleLemonYellow,
+            modifier = Modifier.padding(start = 16.dp,)
+        )
+        Box( modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.heroimage),
+                contentDescription = "hero image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth(0.35f)
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(8.dp))
+                    .align(Alignment.BottomEnd)
+            )
+            Column() {
+                Text(
+                    text = "Little Lemon",
+                    style = MaterialTheme.typography.h1,
+                    color = LittleLemonYellow,
+                    modifier = Modifier.padding(start = 16.dp,)
+                )
+                Text(
+                    text = "Little Lemon",
+                    style = MaterialTheme.typography.h1,
+                    color = LittleLemonYellow,
+                    modifier = Modifier.padding(start = 16.dp,)
+                )
+            }
+        }
 
 
+    }
 }
--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.littlelemon
 
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,17 +13,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    //value  for SharedPreferences
     private val loginLiveData = MutableLiveData<Boolean>()
     private val userFirstNameLiveData = MutableLiveData<String>()
     private val userLastNameLiveData = MutableLiveData<String>()
     private val userEmailLiveData = MutableLiveData<String>()
+    //get  SharedPreferences
     private val sharedPreferences by lazy {
         getSharedPreferences("LittleLemon", MODE_PRIVATE)
     }
+
+    //instance for http client
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    //instance for Room database
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
@@ -32,16 +60,54 @@ class MainActivity : ComponentActivity() {
         userEmailLiveData.value = sharedPreferences.getString("userEmail", "")
         setContent {
             LittleLemonTheme {
+                println ("some text");
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    //get menu from server
+
+                    //load database
+                    //val databaseMenuItems by database.menuItemDao().getAll().observeAsState(emptyList())
+
+
+
+                    //create a navigation
                     val navController = rememberNavController()
                     Navigation(navController)
+
+
                 }
             }
         }
+
+
+        //if database is empty download database from server and save it
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                val menuItemsNetwork = fetchMenu()
+                saveMenuToDatabase(menuItemsNetwork)
+            }
+        }
+
+
     }
+
+    //get menu from server
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        return httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body<MenuNetworkdata>()
+            .menu
+    }
+
+    //save menu to database
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+    }
+
 }
 
